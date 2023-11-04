b69c0a2a6d3267dc7778d5a4e28e0ad72be7dbe0
hendridg <hendridg@gmail.com>
2023-11-04 13:10:46 -0400

    Add support for draft status in InvoiceStatus component

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,7 +1,11 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
-export default function InvoiceStatus({ status }: { status: string }) {
+export default function InvoiceStatus({
+  status,
+}: {
+  status: 'paid' | 'pending' | 'draft';
+}) {
   return (
     <span
       className={clsx(
@@ -9,6 +13,7 @@ export default function InvoiceStatus({ status }: { status: string }) {
         {
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
+          'bg-orange-300 text-orange-700': status === 'draft',
         },
       )}
     >
@@ -22,6 +27,12 @@ export default function InvoiceStatus({ status }: { status: string }) {
         <>
           Paid
           <CheckIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+      {status === 'draft' ? (
+        <>
+          Draft
+          <CheckIcon className="ml-1 w-4 text-orange-700" />
         </>
       ) : null}
     </span>

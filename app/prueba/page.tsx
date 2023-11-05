5bd7997f41f4ad89ce75711650ae428a71ea7e34
hendridg <hendridg@gmail.com>
2023-11-05 18:52:29 -0400

    Add prueba page component

---
+++ app/prueba/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold text-center text-gray-800">prueba</h1>
+    </div>
+  );
+};
+
+export default Page;
